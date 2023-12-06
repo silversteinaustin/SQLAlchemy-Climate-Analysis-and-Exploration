35a023a7e8b6085824295c4b5f9b8f9cca51057c
Austin Silverstein <silversteinaustin@gmail.com>
2023-12-05 22:06:28 -0500

    Finished Flask

--- Surfsup/app.py
+++ Surfsup/app.py
@@ -1,36 +1,149 @@
-# Import the dependencies.
 from flask import Flask, jsonify
 import numpy as np
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(base_dir, "Resources", "hawaii.sqlite")
+engine = create_engine(f"sqlite:///{db_path}")
+
+# Create a Flask application
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
+# Define a route and a view function
+@app.route('/')
+def date_prev_year():
+    # Create the session
+    session = Session(engine)
+
+    # Define the most recent date in the Measurement dataset
+    # Then use the most recent date to calculate the date one year from the last date
+    most_recent_date = session.query(func.max(Measurement.date)).first()[0]
+    first_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d") - dt.timedelta(days=365)
+
+    # Close the session                   
+    session.close()
+
+    # Return the date
+    return(first_date)
+
+# Define what to do when the user hits the homepage
 @app.route("/")
-def home():
-    if __name__ == "__main__":
-        app.run(debug=True)
+def homepage():
+    return """ <h1> Welcome to Honolulu, Hawaii Climate API! </h1>
+    <h3> The available routes are: </h3>
+    <ul>
+    <li><a href = "/api/v1.0/precipitation"> Precipitation</a>: <strong>/api/v1.0/precipitation</strong> </li>
+    <li><a href = "/api/v1.0/stations"> Stations </a>: <strong>/api/v1.0/stations</strong></li>
+    <li><a href = "/api/v1.0/tobs"> TOBS </a>: <strong>/api/v1.0/tobs</strong></li>
+    <li>To retrieve the minimum, average, and maximum temperatures for a specific start date, use <strong>/api/v1.0/&ltstart&gt</strong> (replace start date in yyyy-mm-dd format)</li>
+    <li>To retrieve the minimum, average, and maximum temperatures for a specific start-end range, use <strong>/api/v1.0/&ltstart&gt/&ltend&gt</strong> (replace start and end date in yyyy-mm-dd format)</li>
+    </ul>
+    """
 
-# reflect an existing database into a new model
+# Define what to do when the user hits the precipitation URL
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create the session
+    session = Session(engine)
 
-# reflect the tables
+    # Query precipitation data from last 12 months from the most recent date from Measurement table
+    prcp_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= date_prev_year()).all()
+    
+    # Close the session                   
+    session.close()
 
+    # Create a dictionary from the row data and append to a list of prcp_list
+    prcp_list = []
+    for date, prcp in prcp_data:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        prcp_list.append(prcp_dict)
 
-# Save references to each table
+    # Return a list of jsonified precipitation data for the previous 12 months 
+    return jsonify(prcp_list)
 
+# Define what to do when the user hits the station URL
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create the session
+    session = Session(engine)
 
-# Create our session (link) from Python to the DB
+    # Query station data from the Station dataset
+    station_dataset = session.query(Station.station).all()
 
+    # Close the session                   
+    session.close()
 
-#################################################
-# Flask Setup
-#################################################
+    # Convert list of tuples into normal list
+    station_list = list(np.ravel(station_dataset))
 
+    # Return a list of jsonified station data
+    return jsonify(station_list)
 
+# Define what to do when the user hits the URL
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session
+    session = Session(engine)
 
-#################################################
-# Flask Routes
-#################################################
+    # Query tobs data from last 12 months from the most recent date from Measurement table
+    tobs_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').\
+                        filter(Measurement.date >= date_prev_year()).all()
+
+    # Close the session                   
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of tobs_list
+    tobs_list = []
+    for date, tobs in tobs_data:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_list.append(tobs_dict)
+
+    # Return a list of jsonified tobs data for the previous 12 months
+    return jsonify(tobs_list)
+
+# Define what to do when the user hits the URL with a specific start date or start-end range
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def cal_temp(start=None, end=None):
+    # Create the session
+    session = Session(engine)
+    
+    # Make a list to query (the minimum, average and maximum temperature)
+    sel=[func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    
+    # Check if there is an end date then do the task accordingly
+    if end == None: 
+        # Query the data from start date to the most recent date
+        start_data = session.query(*sel).\
+                            filter(Measurement.date >= start).all()
+        # Convert list of tuples into normal list
+        start_list = list(np.ravel(start_data))
+
+        # Return a list of jsonified minimum, average and maximum temperatures for a specific start date
+        return jsonify(start_list)
+    else:
+        # Query the data from start date to the end date
+        start_end_data = session.query(*sel).\
+                            filter(Measurement.date >= start).\
+                            filter(Measurement.date <= end).all()
+        # Convert list of tuples into normal list
+        start_end_list = list(np.ravel(start_end_data))
+
+        # Return a list of jsonified minimum, average and maximum temperatures for a specific start-end date range
+        return jsonify(start_end_list)
+
+    # Close the session                   
+    session.close()
+    
+# Define main branch 
+if __name__ == "__main__":
+    app.run(debug = True)
